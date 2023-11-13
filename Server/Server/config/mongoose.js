87a64bf25444335b4cc1b98ee84191c7b2513189
CesarZacatales <00223021@uca.edu.sv>
2023-11-12 23:36:28 -0600

    pruebasSED

---
+++ Server/Server/config/mongoose.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const debug = require("debug")("app:mongoose");
+const { MongoClient } = require('mongodb');
+
+const dbhost = process.env.DBHOST || "localhost";
+const dbport = process.env.DBPORT || "3000";
+const dbname = process.env.DBNAME || "wdyt-uca";
+
+const dburi = 'mongodb://localhost:3000/wdyt-uca/wdyt-uca';
+
+const connect = async () => {
+  debug(dburi);
+  try {
+    await mongoose.connect(dburi);
+    debug("Conexión a la base exitosa");
+  } catch (error) {
+    debug("Error en la conexión de la base");
+    process.exit(1);
+  }
+}
+
+module.exports = {
+  connect
+}
+
